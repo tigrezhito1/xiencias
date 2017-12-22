8e60747c304d4be38901302be2e9d63c9443bf55
root <root@mars>
2017-12-22 16:20:32 -0500

    cambios

---
+++ a/peliculas.js
@@ -0,0 +1,48 @@
+{
+	"peliculas": [
+
+	{
+		"id": 1,
+		"nombre": "el sexto sentido",
+		"director": "mm",
+		"clasificacon": "drama"
+	},
+
+	{
+		"id": 2,
+		"nombre": "sentido",
+		"director": "r.mmfdg",
+		"clasificacon": "accion"
+	},
+
+	{
+		"id": 3,
+		"nombre": "titanic",
+		"director": "rtf",
+		"clasificacon": "documental"
+	},
+
+],
+
+{
+	"clasificacion": [
+
+	{
+		
+		"nombre": "drama",
+		"id": 1
+
+	{
+		
+		"nombre": "accion",
+		"id": 2
+		
+
+	{
+		
+		"nombre": "documental",
+		"id": 3
+	
+	},
+
+],
